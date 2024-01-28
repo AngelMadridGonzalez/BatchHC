0937c0055beea791fdeaf1769e0cfab5e305dd2d
angelLuisMadrid <madrid.angel.luis@gmail.com>
2024-01-28 12:00:25 +0100

    Empty project

--- src/main/java/org/hc/model/repository/CuentasClientesRepository.java
+++
@@ -1,7 +0,0 @@
-package org.hc.model.repository;
-
-import org.hc.model.entity.CuentasClientes;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface CuentasClientesRepository extends JpaRepository<CuentasClientes, Integer> {
-
-}
