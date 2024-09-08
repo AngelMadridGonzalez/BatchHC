07db1b66db41b8dacf55e5347e535c1bc72ae4af
angelLuisMadrid <madrid.angel.luis@gmail.com>
2024-09-08 14:20:42 +0200

    only animals and clients

--- src/main/java/org/hc/ImportadorCSV.java
+++ src/main/java/org/hc/ImportadorCSV.java
@@ -4,19 +4,12 @@ import java.io.IOException;
 
 import static org.hc.service.AnimalService.Animales;
 import static org.hc.service.ClienteService.Clientes;
-import static org.hc.service.CuentaClienteService.CuentasClientes;
-import static org.hc.service.CuentaClienteService.CuentasClientesLineas;
-import static org.hc.service.TicketCobroService.TicketCobros;
-import static org.hc.service.ReservaServiciosService.ReservasServicios;
+
 public class ImportadorCSV {
     public static void main(String[] args) throws IOException {
         System.out.println("INI");
         Clientes();
         Animales();
-        //CuentasClientes();
-        //CuentasClientesLineas();
-        //ReservasServicios();
-        //TicketCobros();
         System.out.println("FIN");
     }
 }
